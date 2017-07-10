0f39606064723e0a8c252f836019edfe3a4349a5
Peter <peter.chang@chaogun.com>
2017-07-10 16:01:09 +0800

    Initialize

---
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Configuration;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Bot_Application1.Dialogs {
+    [Serializable]
+    public class RootDialog : IDialog<object> {
+        public Task StartAsync(IDialogContext context) {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result) {
+            
+            var activity = await result as Activity;
+
+            string[] separators = { ",", "!", ";", " " };
+
+            string keyword = ConfigurationManager.AppSettings["keyword"];
+            List<string> keywordList = keyword.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+
+            string send = ConfigurationManager.AppSettings["send"];
+            List<string> sendList = send.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+
+            int index = keywordList.FindIndex(T => T.Contains(activity.Text));
+            if (index != -1) {
+                await context.PostAsync(sendList[index]);
+            }
+
+            
+            else {
+                // calculate something for us to return
+                int length = (activity.Text ?? string.Empty).Length;
+                // return our reply to the user
+                //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+                await context.PostAsync("test");
+            }
+
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
