3a3fd45f43ff52e8d1ea7b7233ae05bd78842c63
Peter <peter.chang@chaogun.com>
2017-07-10 16:11:53 +0800

    修改

--- Bot Application1/Dialogs/RootDialog.cs
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -17,32 +17,40 @@ namespace Bot_Application1.Dialogs {
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result) {
             
-            var activity = await result as Activity;
+            try{
+                var activity = await result as Activity;
 
-            string[] separators = { ",", "!", ";", " " };
+                string[] separators = { ",", "!", ";", " " };
 
-            string keyword = ConfigurationManager.AppSettings["keyword"];
-            List<string> keywordList = keyword.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+                string keyword = ConfigurationManager.AppSettings["keyword"];
+                List<string> keywordList = keyword.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
 
-            string send = ConfigurationManager.AppSettings["send"];
-            List<string> sendList = send.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
+                string send = ConfigurationManager.AppSettings["send"];
+                List<string> sendList = send.Split(separators, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).Where(x => x.Length > 0).ToList();
 
-            int index = keywordList.FindIndex(T => T.Contains(activity.Text));
-            if (index != -1) {
-                await context.PostAsync(sendList[index]);
+                if(activity.Text.Contains("123")){
+                    DateTime dayOff = new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, 18, 0, 0);
+                    double dd = dayOff.Subtract(DateTime.UtcNow.AddHours(8)).TotalMinutes;
+                    await context.PostAsync("倒數"+dd.ToString()+"分鐘");
+                }
+
+                int index = keywordList.FindIndex(T => T.Contains(activity.Text));
+                if (index != -1) {
+                    await context.PostAsync(sendList[index]);
+                } else {
+                    // calculate something for us to return
+                    int length = (activity.Text ?? string.Empty).Length;
+                    // return our reply to the user
+                    //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+                    await context.PostAsync("test");
+                }
+
+
+                context.Wait(MessageReceivedAsync);
+            } catch(Exception ex){
+                await context.PostAsync(ex.Message);
             }
-
-            
-            else {
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-                // return our reply to the user
-                //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
-                await context.PostAsync("test");
-            }
-
-
-            context.Wait(MessageReceivedAsync);
+           
         }
     }
 }
