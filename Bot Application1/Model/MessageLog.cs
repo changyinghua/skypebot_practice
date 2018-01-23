39cd890a5e6aa248b53da8956c0765672ed1a784
peter <a85060421@gmail.com>
2018-01-23 20:57:58 +0800

    update

---
+++ Bot Application1/Model/MessageLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application1.Model {
+    public class MessageLog {
+        public string Name { get; set; }
+        public string Log { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
